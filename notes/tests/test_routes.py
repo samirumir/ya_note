5101f8a92f553b35cd005b4e7be937d48d311135
Samir <148135908+samirumir@users.noreply.github.com>
2024-03-30 23:35:00 +0300

    скопированный код из практикума

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -3,11 +3,10 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import redirect
 from django.test import TestCase
 from django.urls import reverse
+from yanote.wsgi import *
 
-# Импортируем класс комментария.
 from notes.models import Note
 
-# Получаем модель пользователя.
 User = get_user_model()
 
 
@@ -23,8 +22,20 @@ class TestRoutes(TestCase):
             text='Текст',
             author=cls.author)
 
+    def test_availability_homepage_for_anonymous(self):
+        """Главная страница доступна анонимному пользователю"""
+        pass
+
+
+    def test_availability_for_authorized_client(self):
+        """Аутентифицированному пользователю доступна страницы
+        notes:list, note:done, nots:add"""
+        pass
+
 
     def test_availability_for_note_edit_and_delete(self):
+        """Проверка на редактирование(удаление) авторизованному
+         и неавторизованнуому пользователю"""
         users_statuses = (
             (self.author, HTTPStatus.OK),
             (self.reader, HTTPStatus.NOT_FOUND),
@@ -39,13 +50,20 @@ class TestRoutes(TestCase):
                     url = reverse(name, args=(self.notes.slug,))
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
-                    # Проверка на редирект после редактирования и удаления записей
-                    # if response.status_code == 200:
-                    #     self.assertRedirects(response, 'notes:success')
+
 
     def test_redirect_for_anonymous_client(self):
+        """Редирект неавторизованных пользователей на страницу регистрации"""
         login_url = reverse('users:login')
-        for name in ('notes:edit', 'notes:delete'):
+        for name in (
+            'notes:edit',
+            'notes:delete',
+            'notes:done',
+            'notes:add',
+            'notes:detail',
+            'notes:list',
+            'notes:success',
+            ):
             with self.subTest(name=name):
                 # Получаем адрес страницы редактирования или удаления комментария:
                 url = reverse(name, args=(self.notes.slug,))
@@ -58,17 +76,8 @@ class TestRoutes(TestCase):
                 # Проверяем, что редирект приведёт именно на указанную ссылку.
                 self.assertRedirects(response, redirect_url)
 
-    def test_redirect_after_registration_signup(self):
-        login_redirect_url = reverse('notes:home')
-        for name in ('users:login', 'users:signup'):
-            with self.subTest(name=name):
-                # Получаем адрес страницы редактирования или удаления комментария:
-                url = redirect(name)
-                # Получаем ожидаемый адрес страницы логина,
-                # на который будет перенаправлен пользователь.
-                # Учитываем, что в адресе будет параметр next, в котором передаётся
-                # адрес страницы, с которой пользователь был переадресован.
-                redirect_url = f'{login_redirect_url}'
-                response = self.client.get(url)
-                # Проверяем, что редирект приведёт именно на указанную ссылку.
-                self.assertRedirects(response, redirect_url)
+
+    def test_availability_authpage_for_all_users(self):
+        """Страницы регистрации пользователей,
+         входа в учётную запись и выхода из неё доступны всем пользователям."""
+        pass
